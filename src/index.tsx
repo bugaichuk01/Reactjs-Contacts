fb0d05dce879e745acc0d3ef8c12f176040844f5
bugaichuk01 <bugaichuk001@gmail.com>
2022-08-15 15:15:16 +0300

    Refactor main index.tsx file with redux-toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
-import store from "./store/store";
+import {setupStore} from "./store/store";
 import {BrowserRouter as Router} from 'react-router-dom'
+
+const store = setupStore();
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
+
 root.render(
     <React.StrictMode>
         <Provider store={store}>
@@ -18,8 +20,3 @@ root.render(
         </Provider>
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
