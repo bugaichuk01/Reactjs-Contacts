1d710efbb96c133cf0ec12ce02a9f20d7391281e
bugaichuk01 <bugaichuk001@gmail.com>
2022-08-19 18:10:00 +0300

    Little refactor

--- src/store/reducers/authSlice.ts
+++ src/store/reducers/authSlice.ts
@@ -14,12 +14,18 @@ export const authSlice = createSlice({
     extraReducers(builder) {
         builder.addCase(login.fulfilled, (state, action: PayloadAction<boolean>) => {
             state.auth = action.payload;
+            state.error = '';
+            state.loading = false;
         })
         builder.addCase(login.pending, (state) => {
             state.loading= true;
+            state.error = '';
+            state.auth = false;
         })
         builder.addCase(login.rejected, (state) => {
             state.error = 'Логин/пароль неверен';
+            state.loading = false;
+            state.auth = false;
         })
 }})
 
