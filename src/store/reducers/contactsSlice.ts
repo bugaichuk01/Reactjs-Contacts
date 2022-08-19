0e26fa031bac427950ff59dca5d0b1a787aacbe7
bugaichuk01 <bugaichuk001@gmail.com>
2022-08-19 17:56:12 +0300

    Refactor from reducers to extraReducers

--- src/store/reducers/contactsSlice.ts
+++ src/store/reducers/contactsSlice.ts
@@ -2,10 +2,11 @@ import {
     IContact, ContactsState
 } from "../../types/Contacts";
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {addContact, deleteContact, editContact, fetchContacts} from "../actions/contactsActions";
 
 const initialState: ContactsState = {
-    contacts: [],
-    filtered: [],
+    contacts: null,
+    filtered: null,
     loading: false,
     error: '',
 }
@@ -14,37 +15,45 @@ export const contactsSlice = createSlice({
     name: 'contacts',
     initialState,
     reducers: {
-        fetchContacts(state) {
-            state.loading = true;
-        },
-        fetchContactsSuccess(state, action: PayloadAction<IContact[]>) {
-            state.contacts = action.payload;
-            state.filtered = action.payload;
-            state.loading = false;
-        },
-        fetchContactsError(state, action: PayloadAction<string>) {
-            state.error = action.payload;
-            state.loading = false;
-        },
-        addContact(state, action: PayloadAction<IContact>) {
-            state.contacts.push(action.payload);
-            state.filtered.push(action.payload);
-        },
-        editContact(state, action: PayloadAction<IContact>) {
-            state.contacts = state.contacts.map((contact) => (contact.id === action.payload.id ? action.payload : contact));
-            state.filtered = state.filtered.map((contact) => (contact.id === action.payload.id ? action.payload : contact));
-        },
-        deleteContact(state, action: PayloadAction<number>) {
-            state.contacts = state.contacts.filter((contact) => contact.id !== action.payload);
-            state.filtered = state.filtered.filter((contact) => contact.id !== action.payload);
-        },
         searchContact(state, action: PayloadAction<string>) {
+            if (!state.contacts || !state.filtered) return
             state.filtered = state.contacts.filter((contact) => {
                 return Object.keys(contact).some((key) =>
                     contact[key].toString().toLowerCase().includes(action.payload)
                 );
             })
         }
+    },
+    extraReducers(builder) {
+        builder.addCase(fetchContacts.fulfilled, (state, action: PayloadAction<IContact[]>) => {
+            state.contacts = action.payload;
+            state.filtered = state.contacts;
+            state.loading = false;
+        })
+        builder.addCase(fetchContacts.pending, (state) => {
+            state.loading = true;
+            state.error = '';
+        })
+        builder.addCase(fetchContacts.rejected, (state, action) => {
+            state.error = action.error.message;
+            state.contacts = null;
+            state.loading = false;
+        })
+        builder.addCase(addContact.fulfilled, (state, action: PayloadAction<IContact>) => {
+            if (!state.contacts || !state.filtered) return
+            state.contacts.push(action.payload);
+            state.filtered = state.contacts;
+        })
+        builder.addCase(editContact.fulfilled, (state, action: PayloadAction<IContact>) => {
+            if (!state.contacts || !state.filtered) return
+            state.contacts = state.contacts.map((contact) => (contact.id === action.payload.id ? action.payload : contact));
+            state.filtered = state.contacts;
+        })
+        builder.addCase(deleteContact.fulfilled, (state, action: PayloadAction<number>) => {
+            if (!state.contacts || !state.filtered) return
+            state.contacts = state.contacts.filter((contact) => contact.id !== action.payload);
+            state.filtered = state.contacts;
+        })
     }
 })
 
